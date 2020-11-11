5a0323f5ad03c67dc1a4c7de97d2897d08b08bd4
RPXIC <rpc2works@gmail.com>
2020-11-11 10:13:41 +0100

    first push

---
+++ src/data/index.ts
@@ -0,0 +1,11 @@
+import { CircuitsData } from './data-circuits';
+import { RacesData } from './data-races';
+import { SeasonsData } from './data-seasons';
+import { DriversData } from './data-drivers';
+
+export const dataSources = {
+    SeasonsData,
+    RacesData,
+    DriversData,
+    CircuitsData
+};
